43f42d63cce1c6b15b4b3015339a02f6583d7b53
Saif <saifalislam2022@gmail.com>
2025-06-25 12:25:46 +0600

    feat: creating ResponseImpl skull

--- src/framework/Response.ts
+++ src/framework/Response.ts
@@ -1,24 +1,4 @@
 import { IncomingMessage, ServerResponse } from "node:http";
 import { Response } from "./type";
 
-export class ResponseImpl implements Response {
-  statusCode: number = 200;
-  headers: Record<string, string> = {};
-
-  constructor(public nodeRes: ServerResponse<IncomingMessage>) {}
-
-  status(code: number): this {
-    this.statusCode = code;
-    return this;
-  }
-  setHeader(key: string, value: string): this {
-    this.headers[key] = value;
-    return this;
-  }
-  send(body?: string | object | Buffer | null): this {
-    throw new Error("Method not implemented.");
-  }
-  json(body: object): this {
-    throw new Error("Method not implemented.");
-  }
-}
+export class ResponseImpl implements Response {}
